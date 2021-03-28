f590457a860dbc5a1ae0317f900b44e2a66eae55
Yagnik Virani <77527826+viraniyagnik@users.noreply.github.com>
2021-03-28 15:20:17 -0400

    GetAllStudent
    Through this task I fetched all student list.

---
+++ smss/smss/Views/GetAllStudent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using smss.Models;
+using SQLite;
+
+using Xamarin.Forms;
+
+namespace smss.Views
+{
+    public class GetAllStudent : ContentPage
+    {
+        private ListView listView;
+        string _dbpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "myDB.db");
+        public GetAllStudent()
+        {
+            this.Title = "Student List";
+            var db = new SQLiteConnection(_dbpath);
+
+            StackLayout stackLayout = new StackLayout();
+
+
+            listView = new ListView();
+            listView.ItemsSource = db.Table<Student_list>().OrderBy(x => x.Student_name).ToList();
+            stackLayout.Children.Add(listView);
+
+            Content = stackLayout;
+        }
+    }
+}
