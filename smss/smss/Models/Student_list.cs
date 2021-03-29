e9eaee1f92c9d49e383c609d7eac181165afe7d0
Yagnik Virani <77527826+viraniyagnik@users.noreply.github.com>
2021-03-29 16:38:48 -0400

    Student list

---
+++ smss/smss/Models/Student_list.cs
@@ -0,0 +1,24 @@
+﻿
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace smss.Models
+{
+    public class Student_list
+    {
+        [PrimaryKey]
+        public int Student_id { get; set; }
+        public string Student_name { get; set; }
+        public string Student_course { get; set; }
+        public string Student_department { get; set; }
+        public string Student_status { get; set; }
+
+        public override string ToString()
+        {
+            return this.Student_name + "( " + this.Student_course + ",  " + this.Student_department + ",  " + Student_status + "  )" ;
+        }
+    }
+}
