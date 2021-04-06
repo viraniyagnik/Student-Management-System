1c8f47ad1da95768e7a8452568018f9c8d9baca2
Yagnik Virani <77527826+viraniyagnik@users.noreply.github.com>
2021-04-06 15:37:26 -0400

    Add time table page

---
+++ smss/smss/Views/TimeTable.xaml.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using smss.ViewModels;
+
+namespace smss.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TimeTable : ContentPage
+    {
+        public TimeTable()
+        {
+            InitializeComponent();
+
+            BindingContext = new TimeTableViewModel();
+
+            var Table = new Image()
+            {
+                Source = "time_table.png"
+            };
+
+
+            Content = new StackLayout() { Padding = 05, Children = { Table } };
+        }
+    }
+}
