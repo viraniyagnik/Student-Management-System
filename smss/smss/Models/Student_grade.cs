55ad2ec2a1ba5b0e660eb388a0f204a2593ec7db
Yagnik Virani <77527826+viraniyagnik@users.noreply.github.com>
2021-04-09 19:52:41 -0400

    grade report

---
+++ smss/smss/Models/Student_grade.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace smss.Models
+{
+    class Student_grade
+    {
+        [PrimaryKey]
+        public int Student_id { get; set; }
+        public string Student_name { get; set; }
+        public string Student_subject1 { get; set; }
+        public string Student_subject2 { get; set; }
+        public string Student_subject3 { get; set; }
+
+       
+        public override string ToString()
+        {
+            return this.Student_name + "(oop : " + Student_subject1 + ", Maths :  " + Student_subject2 + ", Java : " + Student_subject3 + "  )";
+        }
+    }
+}
